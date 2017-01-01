8752d99e7849b5c039d022d135de0474ca28af12
Chino Kim <chjno.k@gmail.com>
2017-01-01 16:44:40 -0500

    ===, comments

--- 00_hide_timeline/content_script.js
+++ 00_hide_timeline/content_script.js
@@ -1,19 +1,9 @@
 var observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
-        // console.log('new elements');
-        // console.log(mutation)
-        // if (mutation.addedNodes && mutation.addedNodes.length > 0) {
-        //     // element added to DOM
-        //     console.log('new elements');
-        // }
 
         var divs = document.getElementsByTagName('div');
-
         for (var i = 0; i < divs.length; i++){
-            if (divs[i].id && divs[i].id.indexOf("hyperfeed_story_id_") == 0){
-                // console.log(divs[i]);
-                // pagerPagelet = divs[i];
-                // divs[i].parentElement.removeChild(divs[i]);
+            if (divs[i].id && divs[i].id.indexOf("hyperfeed_story_id_") === 0){
                 divs[i].style.visibility = 'hidden';
             }
         }
@@ -31,7 +21,7 @@ observer.observe(document.body, config);
 var divs = document.getElementsByTagName('div');
 
 for (var i = 0; i < divs.length; i++){
-    if (divs[i].id && divs[i].id.indexOf("topnews_main_stream_") == 0){
+    if (divs[i].id && divs[i].id.indexOf("topnews_main_stream_") === 0){
       divs[i].style.visibility = 'hidden';
     }
 }
