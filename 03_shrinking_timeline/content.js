0e6585e4260f558b1b55f9c30373b70a65ef5be1
Chino Kim <chjno.k@gmail.com>
2017-01-04 14:36:46 -0500

    03 done

--- 03_shrinking_timeline/content.js
+++ 03_shrinking_timeline/content.js
@@ -1,13 +1,37 @@
+var posts = [];
+var postIndex = 0;
+
 var getPosts = function(){
-  var posts = [];
+  posts = [];
   var divs = document.getElementsByTagName('div');
   for (var i = 0; i < divs.length; i++){
-    if (divs[i].id.indexOf('hyperfeed_story_id_') != -1){
+    if (divs[i].id && divs[i].id.indexOf('hyperfeed_story_id_') != -1){
       posts.push(divs[i]);
     }
   }
-  
-  return posts;
+  shrinkPosts();
 };
 
-posts[0].setAttribute('style','transform:scale(.5);');
+var shrinkPosts = function(){
+  for (var i = postIndex; i < posts.length; i++){
+    if (postIndex > 0){
+      posts[i].setAttribute('style','transform:scale(' + (1 - Math.tanh(.1 * postIndex)) + ');');
+    }    
+    postIndex++;
+  }
+};
+
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      getPosts();
+    });
+});
+
+var config = {
+    attributes: true,
+    childList: true,
+    characterData: true
+};
+
+getPosts();
+observer.observe(document.body, config);
