e3eb18c7d5fd6f102040d577374d21f6653198dc
Chino Kim <chjno.k@gmail.com>
2017-01-04 17:55:23 -0500

    rm palette file after conversion

--- mov2gif.sh
+++ mov2gif.sh
@@ -3,3 +3,5 @@ filters="fps=10,scale=-1:500:flags=lanczos,setpts=$3*PTS"
 
 ffmpeg -i $1 -vf "$filters,palettegen" -y $palette
 ffmpeg -i $1 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $2.gif
+rm $palette
+
