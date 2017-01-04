6aae60f50d73f56253d0a3121bd5f0418a8a5457
Chino Kim <chjno.k@gmail.com>
2017-01-04 17:52:11 -0500

    fix gif script + filename args

--- mov2gif.sh
+++ mov2gif.sh
@@ -1,5 +1,5 @@
 palette="palette.png"
-filters="fps=10,scale=-1:500:flags=lanczos,setpts=$1*PTS"
+filters="fps=10,scale=-1:500:flags=lanczos,setpts=$3*PTS"
 
-ffmpeg -i cut.mov -vf "$filters,palettegen" -y $palette
-ffmpeg -i cut.mov -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y output.gif
+ffmpeg -i $1 -vf "$filters,palettegen" -y $palette
+ffmpeg -i $1 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $2.gif
