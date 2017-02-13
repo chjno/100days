65311e047c0a93ec6f055d794deeb7ff22c5f890
Chino Kim <chjno.k@gmail.com>
2017-02-13 00:12:06 -0500

    mutation observer

--- 41_ticking_timeline/content.js
+++ 41_ticking_timeline/content.js
@@ -12,7 +12,7 @@ function checkTime(){
 setInterval(checkTime, 1000);
 
 function update(){
-  degs += 30;
+  degs += 6;
 
   $('._5pcb').find("[id*='hyperfeed_story_id_']").each(function (){
     this.style = 'transform: rotate(' + degs + 'deg);';
@@ -22,3 +22,72 @@ function update(){
     degs = 0;
   }
 }
+
+function processStream(stream){
+  $(stream).find("[id*='hyperfeed_story_id_']").each(function (){
+    this.style = 'transform: rotate(' + degs + 'deg);';
+  });
+}
+
+var streamIds = [];
+function init(callback){
+  var streams = [document.getElementById('substream_0')];
+  var pagelet = streams[0].nextSibling.nextSibling.children[0];
+  streams.push(streams[0].nextSibling);
+  for (var i = 0; i < streams.length; i++){
+    observeStream(streams[i], callback);
+    streamIds.push(streams[i].id);
+  }
+  observePagelet(pagelet, callback);
+}
+
+function observePagelet(elt, callback){
+  var observer = new MutationObserver(function (mutations) {
+    var streams = $('._4ikz');
+    for (var i = 0; i < streams.length; i++){
+      if (streamIds.indexOf(streams[i].id) == -1){
+        observeStream(streams[i], callback);
+        streamIds.push(streams[i].id);
+      }
+    }
+  });
+
+  var config = {
+    attributes: true,
+    childList: true,
+    characterData: true
+  };
+
+  observer.observe(elt, config);
+}
+
+function observeStream(elt, callback){
+  loading = true;
+
+  function stopObserving(){
+    observer.disconnect();
+    callback(elt);
+  }
+
+  var timeout;
+  function setDelay(){
+    clearTimeout(timeout);
+    timeout = setTimeout(stopObserving, 500);
+  }
+
+  var config = {
+    attributes: true,
+    childList: true,
+    characterData: true,
+    subtree: true
+  };
+
+  var observer = new MutationObserver(function (mutations) {
+    setDelay();
+  });
+
+  setDelay();
+  observer.observe(elt, config);
+}
+
+init(processStream);
