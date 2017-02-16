b970ec47a9b6bb0c30db5d1f2e265b1a1892601f
Chino Kim <chjno.k@gmail.com>
2017-02-16 17:50:15 -0500

    46

---
+++ 46_fleeing_links/content.js
@@ -0,0 +1,61 @@
+var fleeIntervals = {};
+
+$('a').hover(onMouseEnter, onMouseLeave);
+
+function onMouseEnter(e){
+  var el = e.target;
+  var width = $(el).width();
+  var height = $(el).height();
+
+  if (!fleeIntervals.hasOwnProperty(el)){
+    var midX = $(el).offset().left + $(el).width()/2 - $(window).scrollLeft();
+    var midY = $(el).offset().top + $(el).height()/2 - $(window).scrollTop();
+    var mouseX = e.clientX;
+    var mouseY = e.clientY;
+
+    var angle = Math.atan2(midY - mouseY, midX - mouseX);
+    var dy = Math.sin(angle);
+    var dx = Math.cos(angle);
+
+    function flee(){
+      if (el.style.transform === ''){
+        el.style.display = 'block';
+        el.style.transform = "matrix(1, 0, 0, 1, 0, 0)";
+      }
+
+      var currentMatrix = el.style.transform;
+      var matrixArr = currentMatrix.replace('matrix(', '').replace(')', '').split(', ');
+      var tx = parseFloat(matrixArr[4]);
+      var ty = parseFloat(matrixArr[5]);
+
+      var deltas = [tx + dx, ty + dy];
+      el.style.transform = 'matrix(1,0,0,1,' + deltas.join(', ') + ')';
+      el.style.width = width + 'px';
+      el.style.height = height + 'px';
+
+      var top = $(el).offset().top - $(window).scrollTop();
+      var bottom = top + $(el).height();
+      var left = $(el).offset().left - $(window).scrollLeft();
+      var right = left + $(el).width();
+    }
+
+    fleeIntervals[el] = setInterval(flee, 4);
+  }
+}
+
+function onMouseLeave(e){
+  var el = e.target;
+  if (fleeIntervals.hasOwnProperty(el)){
+    setTimeout(function (){
+      if (!$(e.target).is(':hover')){
+        clearInterval(fleeIntervals[el]);
+        delete fleeIntervals[el];
+      } else {
+        console.log('still hovering');
+        setTimeout(function (){
+          onMouseLeave(e);
+        }, 200);
+      }
+    }, 500);
+  }
+}
