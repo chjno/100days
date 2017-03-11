31e5544e643f892be21f5dd7fd99b21a17a7d79a
Chino Kim <chjno.k@gmail.com>
2017-03-10 20:10:44 -0500

    wiki

---
+++ wiki/background.js
@@ -0,0 +1,13 @@
+// function getWiki(q){
+//   var url = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=' + q;
+//   $.getJSON(url, function (data) {
+//     console.log(data);
+//   });
+// }
+
+// getWiki('facebook');
+
+
+// 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&redirects=1&titles=imdb'
+
+// The New York Times - Breaking News, World News & Multimedia
