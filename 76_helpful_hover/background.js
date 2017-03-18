d475d44b79e99ef2d1ba9a4968e85f60da56d237
Chino Kim <chjno.k@gmail.com>
2017-03-18 16:43:00 -0400

    use actual duckduckgo api

--- 76_helpful_hover/background.js
+++ 76_helpful_hover/background.js
@@ -1,3 +1,7 @@
+var dict = {
+  // q: results
+};
+
 function duck(msg, tabId){
   var last = false;
   var words = msg.text.split(' ');
@@ -10,63 +14,41 @@ function duck(msg, tabId){
   word = words[randInt(0, words.length)];
   duckgo(word);
 
-  // setTimeout(function (){
-  //   duckgo(words[0]);
-  // }, 200);
+  // duckgo(words[0]);
 
-  function duckgo(q){
-    // console.log(q);
-    $.get('https://duckduckgo.com/?q=' + q, function (data){
-      // d = data;
-      var startString = 'DDG.duckbar.add(';
-      var startIndex = data.indexOf(startString) + startString.length;
-      var d2 = data.substring(startIndex, data.length);
-      var d3 = d2.split('})')[0] + '}';
-
-      try{
-        var obj = JSON.parse(d3);
-        var results = obj['data']['RelatedTopics'];
-        // console.log(results);
-        var resultIndex = randInt(0, results.length);
-        var text = results[resultIndex]['Text'];
-        // var text = results[0]['Text'];
-        if (text){
-          chrome.tabs.sendMessage(tabId, {type: 'title', id: msg.id, text: text});
-        } else {
-          var qIndex = words.indexOf(q);
-          words.splice(qIndex, 1);
-          // words.splice(0, 1);
-          if (words.length > 0){
-            setTimeout(function (){
-              word = words[randInt(0, words.length)];
-              duckgo(word);
-          //     duckgo(words[0]);
-            }, 200);
-          }
-        }
-      }catch(e){
-        // console.log('err');
+  function duckduckgo(q, results){
+    if (results.length > 0){
+      var resultIndex = randInt(0, results.length);
+      var text = results[resultIndex]['Text'];
+      // var text = results[0]['Text'];
+      if (text){
+        console.log(text);
+        chrome.tabs.sendMessage(tabId, {type: 'title', id: msg.id, text: text});
+      } else {
         var qIndex = words.indexOf(q);
         words.splice(qIndex, 1);
         // words.splice(0, 1);
         if (words.length > 0){
-          setTimeout(function (){
-            word = words[randInt(0, words.length)];
-            duckgo(word);
-        //     duckgo(words[0]);
-          }, 200);
+          word = words[randInt(0, words.length)];
+          duckgo(word);
+      //     duckgo(words[0]);
         }
-        // else {
-        //   if (!last){
-        //     var domain = msg.domain;
-        //     var main = domain.split('.');
-        //     main = main[main.length - 2];
-        //     last = true;
-        //     duckgo(main);
-        //   }
-        // }
       }
-    });
+    }
+  }
+
+  function duckgo(q){
+    var results;
+    if (dict[q]){
+      results = dict[q];
+      duckduckgo(q, results);
+    } else {
+      $.getJSON('http://api.duckduckgo.com/?q=' + q + '&format=json', function (data){
+        results = data['RelatedTopics'];
+        dict[q] = results;
+        duckduckgo(q, results);
+      });
+    }
   }
 }
 
