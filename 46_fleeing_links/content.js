b80084d1b1ff14cb9e41d284868ec3298beaf120
Chino Kim <chino.kim@buzzfeed.com>
2025-01-26 17:43:33 -0800

    animation frame

--- 46_fleeing_links/content.js
+++ 46_fleeing_links/content.js
@@ -1,15 +1,16 @@
 var fleeIntervals = {};
+var fleeSpeeds = {};
 
 $('a').hover(onMouseEnter, onMouseLeave);
 
-function onMouseEnter(e){
+function onMouseEnter(e) {
   var el = e.target;
   var width = $(el).width();
   var height = $(el).height();
 
-  if (!fleeIntervals.hasOwnProperty(el)){
-    var midX = $(el).offset().left + $(el).width()/2 - $(window).scrollLeft();
-    var midY = $(el).offset().top + $(el).height()/2 - $(window).scrollTop();
+  if (!fleeIntervals.hasOwnProperty(el)) {
+    var midX = $(el).offset().left + $(el).width() / 2 - $(window).scrollLeft();
+    var midY = $(el).offset().top + $(el).height() / 2 - $(window).scrollTop();
     var mouseX = e.clientX;
     var mouseY = e.clientY;
 
@@ -17,8 +18,11 @@ function onMouseEnter(e){
     var dy = Math.sin(angle);
     var dx = Math.cos(angle);
 
-    function flee(){
-      if (el.style.transform === ''){
+    // Assign a random speed factor
+    fleeSpeeds[el] = Math.random() * 3 + 1;
+
+    function flee() {
+      if (el.style.transform === '') {
         el.style.display = 'block';
         el.style.transform = "matrix(1, 0, 0, 1, 0, 0)";
       }
@@ -28,29 +32,32 @@ function onMouseEnter(e){
       var tx = parseFloat(matrixArr[4]);
       var ty = parseFloat(matrixArr[5]);
 
-      var deltas = [tx + dx, ty + dy];
+      var deltas = [tx + dx * fleeSpeeds[el], ty + dy * fleeSpeeds[el]];
       el.style.transform = 'matrix(1,0,0,1,' + deltas.join(', ') + ')';
       el.style.width = width + 'px';
       el.style.height = height + 'px';
+
+      fleeIntervals[el] = requestAnimationFrame(flee);
     }
 
-    fleeIntervals[el] = setInterval(flee, 4);
+    fleeIntervals[el] = requestAnimationFrame(flee);
   }
 }
 
-function onMouseLeave(e){
+function onMouseLeave(e) {
   var el = e.target;
-  if (fleeIntervals.hasOwnProperty(el)){
-    setTimeout(function (){
-      if (!$(e.target).is(':hover')){
-        clearInterval(fleeIntervals[el]);
+  if (fleeIntervals.hasOwnProperty(el)) {
+    setTimeout(function () {
+      if (!$(e.target).is(':hover')) {
+        cancelAnimationFrame(fleeIntervals[el]);
         delete fleeIntervals[el];
+        delete fleeSpeeds[el];
       } else {
         console.log('still hovering');
-        setTimeout(function (){
+        setTimeout(function () {
           onMouseLeave(e);
         }, 200);
       }
-    }, 500);
+    }, 200);
   }
 }
