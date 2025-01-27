4fc5dcaf35d23a9729ca280278c860117334b4fb
Chino Kim <chino.kim@buzzfeed.com>
2025-01-26 17:48:54 -0800

    element angles, animation frame

--- 59_squeegee/content.js
+++ 59_squeegee/content.js
@@ -1,14 +1,23 @@
-$(window).scroll(function (){
-  $('body :visible').filter(function (){
+$(window).on('scroll', function () {
+  if (!window.requestAnimationFrame) {
+    // Fallback for browsers that do not support requestAnimationFrame
+    setTimeout(updateElements, 16);
+  } else {
+    requestAnimationFrame(updateElements);
+  }
+});
+
+function updateElements() {
+  $('body :visible').filter(function () {
     return ($(this).height() < $(window).height()) && ($(this).height() > 0) && ($(this).width() > 0);
-  }).each(function (){
+  }).each(function () {
     var thisTop = $(this).offset().top;
     var winScrollTop = $(window).scrollTop();
 
     var delta = thisTop - winScrollTop;
 
-    if (delta < 0){
-      if (this.style.transform === ''){
+    if (delta < 0) {
+      if (this.style.transform === '') {
         this.style.transform = "matrix(1, 0, 0, 1, 0, 0)";
       }
 
@@ -16,27 +25,10 @@ $(window).scroll(function (){
       var matrixArr = currentMatrix.replace('matrix(', '').replace(')', '').split(', ');
       var tY = parseFloat(matrixArr[5]) - delta;
 
-      this.style.transform = 'matrix(1,0,0,1,0,' + tY + ')';
+      // Generate a random rotation angle between -5 and 5 degrees
+      var randomRotation = Math.random() * 10 - 5;
 
-      // $(this).find(':visible').filter(function (){
-      //   return ($(this).height() > 0) && ($(this).width() > 0);
-      // }).each(function (){
-      //   if (this.style.transform === ''){
-      //     this.style.transform = "matrix(1, 0, 0, 1, 0, 0)";
-      //   }
-
-      //   var currentMatrix = this.style.transform;
-      //   var matrixArr = currentMatrix.replace('matrix(', '').replace(')', '').split(', ');
-      //   var tY;
-      //   if ($(this).offset().top <= 0){
-      //     tY = parseFloat(matrixArr[5]) - delta;
-      //   } else {
-      //     var innerDelta = $(this).offset().top + delta;
-      //     tY = innerDelta < 0 ? parseFloat(matrixArr[5]) + delta - innerDelta : parseFloat(matrixArr[5]) + delta;
-      //   }
-
-      //   this.style.transform = 'matrix(1,0,0,1,0,' + tY + ')';
-      // });
+      this.style.transform = 'matrix(1,0,0,1,0,' + tY + ') rotate(' + randomRotation + 'deg)';
     }
   });
-});
+}
