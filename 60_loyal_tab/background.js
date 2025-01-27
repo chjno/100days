8e013b6f4b79355af6a323039fa880bc7997a92b
Chino Kim <chino.kim@buzzfeed.com>
2025-01-26 18:03:21 -0800

    fix tab cursor following

--- 60_loyal_tab/background.js
+++ 60_loyal_tab/background.js
@@ -1,26 +1,28 @@
-// 80 px leftBuffer
-// 60 px rightBuffer
-// 230 tab width
+var leftBuffer = 90;
+var tabWidth = 230;
 
 chrome.runtime.onMessage.addListener(function (mouseX, sender, sendResponse) {
-  chrome.windows.getCurrent({populate: true}, function (win){
-    console.log(win);
-
+  chrome.windows.getCurrent({ populate: true }, function (win) {
     var tabCount = win.tabs.length;
-    var tabWidth = (win.width - 140) / tabCount;
-    var newIndex = Math.floor(mouseX / tabWidth);
-    if (newIndex > tabCount - 1){
+
+    var newIndex;
+    if (mouseX > leftBuffer) {
+      newIndex = Math.floor((mouseX - leftBuffer) / tabWidth);
+    } else {
+      newIndex = 0;
+    }
+    if (newIndex > tabCount - 1) {
       newIndex = -1;
     }
 
     var currentTabId;
-    for (var i = 0; i < tabCount; i++){
-      if (win.tabs[i].active){
+    for (var i = 0; i < tabCount; i++) {
+      if (win.tabs[i].active) {
         currentTabId = win.tabs[i].id;
         break;
       }
     }
 
-    chrome.tabs.move(currentTabId, {index: newIndex});
+    chrome.tabs.move(currentTabId, { index: newIndex });
   });
 });
